de70a0f71da24c43802bf87d02061a7a77f8e530
Nicolai Czempin <nicolai.czempin@gmail.com>
2015-05-17 21:17:00 +0200

    version number

--- arcadia/global.h
+++ arcadia/global.h
@@ -5,7 +5,7 @@
 #include <list>
 
 using namespace std;
-const string VERSION = "0.0.1dev";
+const string VERSION = "0.0.1";
 #define USE_UNDERPROMOTION
 vector<string> &split(const string &s, char delim, vector<string> &elems);
 
