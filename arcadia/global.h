44d163b1dfb4c08d67ec44883ff3e25918022054
Nicolai Czempin <nicolai.czempin@gmail.com>
2015-05-17 21:19:59 +0200

    bump version

--- arcadia/global.h
+++ arcadia/global.h
@@ -5,7 +5,7 @@
 #include <list>
 
 using namespace std;
-const string VERSION = "0.0.1";
+const string VERSION = "0.0.2dev";
 #define USE_UNDERPROMOTION
 vector<string> &split(const string &s, char delim, vector<string> &elems);
 
