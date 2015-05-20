4a281e2267421ab1baf44b10c4b1b0a04e0c485b
Nicolai Czempin <nicolai.czempin@gmail.com>
2015-05-20 11:57:48 +0200

    starting with quiescence. Looks very buggy though

--- arcadia/Searcher.cpp
+++ arcadia/Searcher.cpp
@@ -67,3 +67,91 @@ int Searcher::alphabeta(int depth, double extension, Position position, Move mov
 	}
 	return -alpha;
 }
+
+int quiescence_alphabeta(int depth, Position position, Move move, int originalAlpha, int beta, vector<Move> upPv, Position nextPos) {
+		int alpha = originalAlpha;
+		//ValidFlag bestMoveValidFlag = new ValidFlag();
+		//if ((timeUp()) || (timeIsUp)) {
+		//	timeIsUp = true;
+		//	return 0;
+		//}
+		//if (depth > Info.seldepth)
+		//	Info.seldepth = depth;
+		//if (nextPos == null)
+		//	try {
+		//		Info.qs_nodes += 1L;
+		//		nextPos = Position.createPosition(position, move);
+		//	} catch (ThreeRepetitionsAB e) {
+		//		System.err.println("three repetitions. what to do?");
+		//		return 0;
+		//	}
+		int v = Evaluator::getValue(nextPos);
+		if (v >= beta)
+			return -beta;
+		if (v > alpha) {
+			//bestMoveValidFlag.setNr(1);
+			alpha = v;
+		}
+		//kingCapture = false;
+		int loopCount = 0;
+		vector<Move> moves = MoveGenerator::generateAllCaptures(nextPos);
+
+		for(Move newMove: moves){
+			int capture = newMove.captured;
+			int capturing = abs(position.board[newMove.from]);
+			if (capture == 6) {
+			//	kingCapture = true;
+			//	illegalCount += 1;
+				return -666663;
+			}
+		//	if (!shouldBeIgnored(nextPos, newMove, capture, capturing)) {
+				//moveStack.push(newMove);
+				vector<Move> downPv;
+				int value = quiescence_alphabeta(depth + 1, nextPos, newMove, -beta, -alpha, downPv, nextPos);
+				//if (kingCapture) {
+				//	illegalCount += 1;
+				//	moveStack.pop();
+				//	kingCapture = false;
+				//} else {
+				//	loopCount++;
+					if (value >= beta) {
+						//moveStack.pop();
+						return -beta;
+					}
+					//bestMoveValidFlag.setNr(-1);
+					if (value > alpha) {
+						alpha = value;
+						//upPv.clear();
+						//upPv.add(newMove);
+						//upPv.addAll(downPv);
+					}
+					//moveStack.pop();
+			//	}
+			//}
+		}
+	/*	if (loopCount == 0) {
+			nextPos.initNonCaptureMoveGenerator();
+			boolean legal = false;
+			while (nextPos.hasNextNonCaptureMove()) {
+				Move testMove = nextPos.nextNonCaptureMove();
+				Info.quiescentMateCheckNodes += 1;
+				Position tp = Position.createTestPosition(nextPos, testMove);
+				if (!tp.isGivingCheck()) {
+					legal = true;
+					break;
+				}
+			}
+			if (!legal) {
+				SortedSet lMoves = nextPos.generateLegalMoves();
+				if (lMoves.size() == 0) {
+					if (nextPos.isMate()) {
+						int value = -90000 + depth;
+						return -value;
+					}
+					return 0;
+				}
+			}
+			alpha = v;
+	}*/	
+		return -alpha;
+	}
