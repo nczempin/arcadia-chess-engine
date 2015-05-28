6a1994c3005b433b3cd763cb661876095e247da5
Nicolai Czempin <nicolai.czempin@gmail.com>
2015-05-28 21:57:20 +0200

    too many endls

--- arcadia/Searcher.cpp
+++ arcadia/Searcher.cpp
@@ -47,7 +47,7 @@ Move Searcher::findBestmove(vector<Move> moves, Position position){
 			cout << "info ";
 			cout << " currmove " << Info::currmove.toString();
 			cout << " currmovenumber " << Info::currmovenumber;
-			cout << " nodes " << Info::nodes << endl;
+			cout << " nodes " << Info::nodes;
 			cout << " nps " << Info::nps << endl;
 
 			//cout << "trying " << move.toString() << endl;
